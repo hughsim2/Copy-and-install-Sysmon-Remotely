812dba8a5766adebb3a0d686bdc32afaade4dd87
hughsim2 <49271645+hughsim2@users.noreply.github.com>
2020-06-19 12:51:37 +1000

    Update Install and copy sysmon with a config file.ps1

--- Install and copy sysmon with a config file.ps1
+++ Install and copy sysmon with a config file.ps1
@@ -1,3 +1,9 @@
+#computers have been setup in a domain environment
+
+#Before running this you will need to check services for WRM, have the WRM ports open in the firewall.
+
+
+
 Copy-Item -Path C:\sysmon1 -Destination "\\10.0.2.15\C$" -Recurse
 
 
@@ -8,6 +14,3 @@ Invoke-Command -ComputerName client -ScriptBlock {
 
 
 
-#check services for WRM
-
-#also execution policy / running as admin
